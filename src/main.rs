2b5a1030b494acaaedae581c25032a2aa6a12937
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-05-23 16:36:23 -0700

    WIP alternative clocks for measuring PPM micros

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use panic_rtt_core::{self, rprintln, rtt_init_print};
 
 use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::digital::v2::{OutputPin, ToggleableOutputPin};
+use embedded_hal::timer::CountDown;
 
 use stm32f4xx_hal as p_hal;
 use p_hal::stm32 as pac;
@@ -13,6 +14,10 @@ use p_hal::stm32 as pac;
 use p_hal::gpio::{GpioExt, ExtiPin};
 use p_hal::rcc::RccExt;
 use p_hal::time::{U32Ext};
+use p_hal::dwt::{ClockDuration, DwtExt};
+use cortex_m::peripheral::DWT;
+
+use p_hal::timer::Timer;
 
 use pac::interrupt;
 
@@ -23,13 +28,9 @@ use ppm_decode::{PpmParser};
 use core::cell::RefCell;
 use cortex_m::interrupt::Mutex;
 use core::ops::DerefMut;
-use stm32f4xx_hal::gpio::{Input, PullUp, Edge};
+use p_hal::gpio::{Input, PullUp, Edge};
 
 type PpmInputPin = p_hal::gpio::gpiob::PB0<p_hal::gpio::Input<stm32f4xx_hal::gpio::PullUp>>;
-
-//static USER_LED_1:  Mutex<RefCell<Option< GpioTypeUserLed1>>> = Mutex::new(RefCell::new(None));
-// Stores the EXTI interrupt configuration
-// static MUTEX_EXTI:  Mutex<RefCell<Option<pac::EXTI>>>  = Mutex::new(RefCell::new(None));
 
 /// Stores the shared input pin
 static PPM_INPUT_PIN: Mutex<RefCell<Option< PpmInputPin >>> = Mutex::new(RefCell::new(None));
@@ -37,12 +38,12 @@ static PPM_INPUT_PIN: Mutex<RefCell<Option< PpmInputPin >>> = Mutex::new(RefCell
 /// Stores the PPM decoder touched by interrupt handlers and main loop
 static PPM_DECODER: Mutex<RefCell<Option<PpmParser>>>  = Mutex::new(RefCell::new(None));
 
-
 /// Verify: clock ticks per microsecond
-const TICKS_PER_MICRO: u32 =   (168/8);
+const TICKS_PER_MICRO: u32 =   2; //(168/8);
+const CYCLES_PER_MICRO: u32 =   12;
 
-/// Initialize peripherals for Pixracer.
-/// Pixracer chip is [STM32F427VIT6 rev.3](http://www.st.com/web/en/catalog/mmc/FM141/SC1169/SS1577/LN1789)
+/// Initialize peripherals for Pixracer. Pixracer chip is:
+/// [STM32F427VIT6 rev.3](http://www.st.com/web/en/catalog/mmc/FM141/SC1169/SS1577/LN1789)
 pub fn setup_peripherals()  -> (
     (
         impl OutputPin + ToggleableOutputPin,
@@ -53,11 +54,11 @@ pub fn setup_peripherals()  -> (
 )
 {
     let mut dp = pac::Peripherals::take().unwrap();
-    let cp = cortex_m::Peripherals::take().unwrap();
-    //TODO verify: need to enable syscfg
-    dp.RCC.apb2enr.write(|w| w.syscfgen().enabled());
+    let mut cp = cortex_m::Peripherals::take().unwrap();
 
-    // Set up the system clock
+
+    // enable system configuration controller clock
+    dp.RCC.apb2enr.write(|w| w.syscfgen().enabled());
     let rcc = dp.RCC.constrain();
     let clocks = rcc
         .cfgr
@@ -75,11 +76,25 @@ pub fn setup_peripherals()  -> (
     let user_led2 = gpiob.pb1.into_push_pull_output(); //green
     let user_led3 = gpiob.pb3.into_push_pull_output(); //blue
 
+    // setup a microsecond timer for timing PPM edges
+    // use cap/comp channel 3 on TIM3
+    //let mut fast_timer = Timer::tim3(dp.TIM3, 1.mhz(), clocks);
+    // You could use the 32-bit TIM2 counter,
+    // prescaled down to 1MHz so ticks were each 1us,
+    // you could use the maximal update interrupt to extend to 64-bit,
+    // but you'd have to create methods to read both 32-bit portions in an atomic fashion;
+
+    //init CYCCNT cortex_m peripheral
+    DWT::unlock();
+    cp.DCB.enable_trace();
+    cp.DWT.enable_cycle_counter();
+    // let _dwt = cp.DWT.constrain(cp.DCB, clocks);
+
     // PPM input:
     // use TIM3 for HRT_TIMER
     // use cap/comp channel 3 on TIM3 for PPM
     // PPM in pin is gpiob.pb0  af2
-    //TODO verify: no need to switch to AF2 ? into_alternate_af2().
+    //TODO verify: no need to switch to AF2 ? (using into_alternate_af2() )
     let mut ppm_in = gpiob.pb0.into_pull_up_input();
     ppm_in.make_interrupt_source(&mut dp.SYSCFG);
     ppm_in.enable_interrupt(&mut dp.EXTI);
@@ -99,6 +114,8 @@ pub fn setup_peripherals()  -> (
     )
 }
 
+/// This interrupt handler should be called whenever
+/// the PPM input pin detects a (rising) edge.
 #[interrupt]
 fn EXTI0() {
     let microtime =
@@ -109,10 +126,17 @@ fn EXTI0() {
             }
 
             // get current time
-            // TODO get clock time in a safer way? this is atomic with no side effects
+            // let cur_cycles = DWT::get_cycle_count();
+            // rprintln!("cycles: {}", cur_cycles);
+            // let micros = cur_cycles/CYCLES_PER_MICRO;
+
+            // // TODO get clock time in a safer way? this is atomic with no side effects
             let cur_sys_ticks = unsafe { (*pac::SYST::ptr()).cvr.read() };
-            // scale ticks to micros before providing to PPM decoder
-            let micros = cur_sys_ticks / TICKS_PER_MICRO;
+            let monotonic_ticks = u32::max_value() - cur_sys_ticks;
+            let micros = monotonic_ticks / TICKS_PER_MICRO;
+
+            rprintln!("ticks: {} micros: {}", monotonic_ticks, micros);
+
             // tell the PPM decoder we got a pulse start
             if let Some(ref mut decoder) = PPM_DECODER.borrow(cs).borrow_mut().deref_mut() {
                 decoder.handle_pulse_start(micros);
@@ -120,7 +144,7 @@ fn EXTI0() {
             micros
         });
 
-    rprintln!("micros: {}", microtime);
+    //rprintln!("micros: {}", microtime);
 }
 
 
